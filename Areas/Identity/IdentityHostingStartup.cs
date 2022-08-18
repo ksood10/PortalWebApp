a9e7aa464abe0fc4433486536bdc7ec9f4243da0
KSood <KSood@KSOOD-LT>
2022-08-18 19:31:23 -0400

    Add project files.

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PortalWebApp.Areas.Identity.Data;
+using PortalWebApp.Data;
+
+[assembly: HostingStartup(typeof(PortalWebApp.Areas.Identity.IdentityHostingStartup))]
+namespace PortalWebApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<PortalWebAppContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DefaultConnection")));
+
+                services.AddDefaultIdentity<PortalWebAppUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<PortalWebAppContext>();
+            });
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortalWebApp.Models
+{
+    [Table("User")]
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string AbbreviatedName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string EmailAddress { get; set; }
+        public int OrganizationID { get; internal set; }
+    }
+}
