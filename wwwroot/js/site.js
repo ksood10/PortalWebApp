d9105aecf27cb64283ed209b26efdef793dc1650
ksood10 <ksood101@gmail.com>
2022-09-19 23:43:59 -0400

    updated form fields

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,8 +2,21 @@
     $("#Development").prop("checked", true);
     $("#ThrottleNum").val("2");
     $("#ThrottleDuration").val("1");
+
+    $("#ThrottleNum").change(function () {
+        if ($('#file1').val().length > 0 && $("#ThrottleNum").val() >= 0 && $("#ThrottleNum").val() <= 100 && $("#ThrottleDuration").val() >= 0 && $("#ThrottleDuration").val() <= 100)
+            $('#UploadBtn').prop("disabled", false);
+        else
+            $('#UploadBtn').prop("disabled", true);
+    });
+    $("#ThrottleDuration").change(function () {
+        if ($('#file1').val().length > 0 && $("#ThrottleNum").val() >= 0 && $("#ThrottleNum").val() <= 100 && $("#ThrottleDuration").val() >= 0 && $("#ThrottleDuration").val() <= 100)
+            $('#UploadBtn').prop("disabled", false);
+        else
+            $('#UploadBtn').prop("disabled", true);
+    });
     $('#file1').change(function () {
-        if ($(this).val().length > 0 ) 
+        if ($(this).val().length > 0 && $("#ThrottleNum").val() >= 0 && $("#ThrottleNum").val() <= 100 && $("#ThrottleDuration").val() >= 0 && $("#ThrottleDuration").val() <=100  ) 
             $('#UploadBtn').prop("disabled", false);
         else 
             $('#UploadBtn').prop("disabled", true);
